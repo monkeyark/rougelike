0677449fd57eaf34b48d05adcc6e371a9b372605
monkeyark <danryw@outlook.com>
2018-12-17 15:07:37 -0800

    change terrain to type of  map element

--- c++/dungeon.h
+++ c++/dungeon.h
@@ -103,7 +103,7 @@ enum
 	NUM_EQUIPMENT,
 };
 
-class Terrain
+class Tile
 {
 	public:
 		char terrain;
@@ -112,7 +112,7 @@ class Terrain
 		int hardness;
 };
 
-class Room
+class Terrain
 {
 	public:
 		int row;
@@ -121,45 +121,22 @@ class Room
 		int height;
 };
 
-class Water
-{
-	public:
-		int row;
-		int col;
-		int width;
-		int height;
-};
+class Room : public Terrain
+{};
 
-class Lava
-{
-	public:
-		int row;
-		int col;
-		int width;
-		int height;
-};
+class Water : public Terrain
+{};
+
+class Lava : public Terrain
+{};
 
 class Item
 {
-	public :
-		Item()
-		{
-			row = 0;
-			col = 0;
-			color = 0;
-			color_display = 0;
-			damage_bonus = 0;
-			vision_bonus = 0;
-			rarity = 0;
-			type = 0;
-		}
-
-
 	public:
 		int row;
 		int col;
 		char symbol;
-		char * name;
+		char *name;
 		char *description;
 		unsigned int color;
 		int color_display;
@@ -269,79 +246,6 @@ class Object
 		bool seen;
 };
 
-/*
-#include <iostream>
-using namespace std;
-
-class LinkedList{
-    // Struct inside the class LinkedList
-    // This is one node which is not needed by the caller. It is just
-    // for internal work.
-    struct Node {
-        int x;
-        Node *next;
-    };
-
-// public member
-public:
-    // constructor
-    LinkedList(){
-        head = NULL; // set head to NULL
-    }
-
-    // destructor
-    ~LinkedList(){
-        Node *next = head;
-
-        while(next) {              // iterate over all elements
-            Node *deleteMe = next;
-            next = next->next;     // save pointer to the next element
-            delete deleteMe;       // delete the current entry
-        }
-    }
-
-    // This prepends a new value at the beginning of the list
-    void addValue(int val){
-        Node *n = new Node();   // create new Node
-        n->x = val;             // set value
-        n->next = head;         // make the node point to the next node.
-                                //  If the list is empty, this is NULL, so the end of the list --> OK
-        head = n;               // last but not least, make the head point at the new node.
-    }
-
-    // returns the first element in the list and deletes the Node.
-    // caution, no error-checking here!
-    int popValue(){
-        Node *n = head;
-        int ret = n->x;
-
-        head = head->next;
-        delete n;
-        return ret;
-    }
-
-// private member
-private:
-    Node *head; // this is the private member variable. It is just a pointer to the first Node
-};
-
-int main() {
-    LinkedList list;
-
-    list.addValue(5);
-    list.addValue(10);
-    list.addValue(20);
-
-    cout << list.popValue() << endl;
-    cout << list.popValue() << endl;
-    cout << list.popValue() << endl;
-    // because there is no error checking in popValue(), the following
-    // is undefined behavior. Probably the program will crash, because
-    // there are no more values in the list.
-    // cout << list.popValue() << endl;
-    return 0;
-}
-*/
 class Dungeon
 {
 	public:
@@ -360,7 +264,7 @@ class Dungeon
 		PC *pc;
 		int cursor_row;
 		int cursor_col;
-		Terrain map[ROW][COL];
+		Tile map[ROW][COL];
 		Queue pq_tunel;
 		Queue pq_nontunel;
 		std::vector<Monster> monster_desc;
@@ -381,8 +285,6 @@ void move_dungeon();
 
 int get_random(int modulus, int min);
 int is_visible_terrain(int i, int j);
-//int is_room_corridor_stair(int row, int col);
-//int is_water_lava(int row, int col);
 int is_inside(int row, int col);
 int is_monster(int row, int col);
 int is_item(int row, int col);
